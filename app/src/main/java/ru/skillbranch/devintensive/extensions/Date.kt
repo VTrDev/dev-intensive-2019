830d3be52d1138472e15928641b9f72543ab4522
Vladimir Trusov <trusov.public@gmail.com>
2019-07-01 22:44:25 +0300

    Tasks Date.format and Date.add

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,10 +1,41 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+fun Date.format(pattern: String="HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
+    var time = this.time
+
+    time += when (units) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+    return this
+}
 
 fun Date.humanizeDiff(date: Date = Date()): String { // ДЗ 1:16, 1:22
     // return "несколько секунд назад:"
     // return "4 дня назад:"
     return "Более года назад:"
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
 }
