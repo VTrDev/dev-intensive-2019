cda295bb2d72ad954c0e2328f06e2cca46e0ff47
Vladimir Trusov <trusov.public@gmail.com>
2019-07-01 20:37:39 +0300

    Task Base Message, AbstractFactory

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.util.*
+
+fun Date.humanizeDiff(date: Date = Date()): String { // ДЗ 1:16, 1:22
+    // return "несколько секунд назад:"
+    // return "4 дня назад:"
+    return "Более года назад:"
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
