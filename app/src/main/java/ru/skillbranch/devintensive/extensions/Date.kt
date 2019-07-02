366da5ccc2a95fe391f17242974e087f4c418b15
Vladimir Trusov <trusov.public@gmail.com>
2019-07-02 21:54:39 +0300

    Task *Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -27,10 +27,53 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String { // ДЗ 1:16, 1:22
-    // return "несколько секунд назад:"
-    // return "4 дня назад:"
-    return "Более года назад:"
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val diff = this.time - date.time
+
+    return when (diff) {
+        in -SECOND .. SECOND -> "только что"
+        in SECOND + 1 .. 45 * SECOND -> "несколько секунд назад"
+        in 45 * SECOND + 1 .. 75 * SECOND -> "минуту назад"
+        in 75 * SECOND + 1 .. 45 * MINUTE -> "${humanizeTimeUnits(diff / MINUTE, TimeUnits.MINUTE)} назад"
+        in 45 * MINUTE + 1 .. 75 * MINUTE -> "час назад"
+        in 75 * MINUTE + 1 .. 22 * HOUR -> "${humanizeTimeUnits(diff / HOUR, TimeUnits.HOUR)} назад"
+        in 22 * HOUR + 1 .. 26 * HOUR -> "день назад"
+        in 26 * HOUR + 1 .. 360 * DAY -> "${humanizeTimeUnits(diff / DAY, TimeUnits.DAY)} назад"
+        in 360 * DAY .. Long.MAX_VALUE -> "более года назад"
+        else -> "неизвестно"
+    }
+}
+
+fun Date.humanizeTimeUnits(value: Long, units: TimeUnits = TimeUnits.SECOND): String {
+
+    val preLastDigit = value % 100 / 10
+
+    if (preLastDigit == 1L) return when (units) {
+        TimeUnits.SECOND -> "$value секунд"
+        TimeUnits.MINUTE -> "$value минут"
+        TimeUnits.HOUR -> "$value часов"
+        TimeUnits.DAY -> "$value дней"
+    }
+
+    when (value % 10) {
+        1L -> return when (units) {
+            TimeUnits.SECOND -> "$value секунду"
+            TimeUnits.MINUTE -> "$value день"
+            TimeUnits.HOUR -> "$value час"
+            TimeUnits.DAY -> "$value день"
+        }
+        in 2..4 -> return when (units) {
+            TimeUnits.SECOND -> "$value секунды"
+            TimeUnits.MINUTE -> "$value минуты"
+            TimeUnits.HOUR -> "$value часа"
+            TimeUnits.DAY -> "$value дня"
+        }
+        else -> return when (units) {
+            TimeUnits.SECOND -> "$value секунд"
+            TimeUnits.MINUTE -> "$value минут"
+            TimeUnits.HOUR -> "$value часов"
+            TimeUnits.DAY -> "$value дней"
+        }
+    }
 }
 
 enum class TimeUnits {
