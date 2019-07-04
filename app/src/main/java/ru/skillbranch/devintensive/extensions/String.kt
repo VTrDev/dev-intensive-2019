5338ae34c449952685ee7e1f440dc3f7cc3b0272
Vladimir Trusov <trusov.public@gmail.com>
2019-07-04 12:49:48 +0300

    Task *String.truncate + Fix TimeUnits.plural()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,6 +1,8 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(length: Int = 16): String {
+    if (length <= 0) return ""
+
     val str = this.trimEnd()
 
     return if (str.length <= length) str
