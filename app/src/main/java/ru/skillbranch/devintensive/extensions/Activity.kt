0f5f4124c801eb872da1766fc3338260b2ed1cc0
Vladimir Trusov <trusov.public@gmail.com>
2019-07-17 18:01:58 +0300

    Task: **Activity.isKeyboardOpen Activity.isKeyboardClosed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,8 +1,12 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToInt
 
 fun Activity.hideKeyboard() {
     val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -13,4 +17,31 @@ fun Activity.hideKeyboard() {
         view = View(this)
     }
     imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+/*
+https://proandroiddev.com/how-to-detect-if-the-android-keyboard-is-open-269b255a90f5
+https://github.com/enyciaa/AndroidKeyboardStateChecker/blob/master/app/src/main/java/com/example/androidkeyboardstatechecker/KeyboardExtensions.kt
+ */
+
+fun Activity.getRootView(): View {
+    return findViewById(android.R.id.content)
+}
+fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+    )
+}
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = getRootView().height - visibleBounds.height()
+    val marginOfError = this.convertDpToPx(50F).roundToInt()
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
 }
