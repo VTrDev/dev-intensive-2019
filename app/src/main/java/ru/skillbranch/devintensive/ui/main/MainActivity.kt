f88c26b17ee4dfc7a7f34e0b9eb19e253eef37bb
Vladimir Trusov <trusov.public@gmail.com>
2019-08-27 14:10:56 +0300

    Lesson 5 (code from video): Create chat list (RecyclerView).

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -0,0 +1,48 @@
+package ru.skillbranch.devintensive.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.ui.adapters.ChatAdapter
+import ru.skillbranch.devintensive.viewmodels.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var chatAdapter: ChatAdapter
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initToolbar()
+        initViews()
+        initViewModel()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+    }
+
+    private fun initViews() {
+
+        chatAdapter = ChatAdapter()
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+
+        with(rv_chat_list) {
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addItemDecoration(divider)
+        }
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.getChatData().observe(this, Observer { chatAdapter.updateDate(it) } )
+    }
+}
