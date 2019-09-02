b1fc1618b9f0454aa739d68520cb50fdbf588b1b
Vladimir Trusov <trusov.public@gmail.com>
2019-09-02 17:14:15 +0300

    Task: *Создание ArchiveActivity

--- app/src/main/java/ru/skillbranch/devintensive/ui/archive/ArchiveActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/archive/ArchiveActivity.kt
@@ -1,13 +1,105 @@
 package ru.skillbranch.devintensive.ui.archive
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_archive.*
+import kotlinx.android.synthetic.main.activity_main.toolbar
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.data.ChatType
+import ru.skillbranch.devintensive.ui.adapters.ChatAdapter
+import ru.skillbranch.devintensive.ui.adapters.ChatItemTouchHelperCallback
+import ru.skillbranch.devintensive.viewmodels.ArchiveViewModel
 
 class ArchiveActivity : AppCompatActivity() {
+
+    private lateinit var chatAdapter: ChatAdapter
+    private lateinit var viewModel: ArchiveViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_archive)
+        initToolbar()
+        initViews()
+        initViewModel()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        val searchItem = menu?.findItem(R.id.action_search)
+        val searchView = searchItem?.actionView as SearchView
+        searchView.queryHint = "Введите название чата"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.handleSearchQuery(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                viewModel.handleSearchQuery(newText)
+                return true
+            }
+
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (item?.itemId == android.R.id.home) {
+            finish()
+            overridePendingTransition(R.anim.idle, R.anim.bottom_down)
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initToolbar() {
+        title = "Архив чатов"
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initViews() {
+
+        chatAdapter = ChatAdapter{
+            if (it.chatType == ChatType.ARCHIVE) {
+                val intent = Intent(this, ArchiveActivity::class.java)
+                startActivity(intent)
+            } else {
+                Snackbar.make(rv_archive_list, "Click on ${it.title}", Snackbar.LENGTH_LONG).show()
+            }
+        }
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        val touchCallback = ChatItemTouchHelperCallback(chatAdapter) { chatItem ->
+            viewModel.restoreFromArchive(chatItem.id)
+            Snackbar.make(rv_archive_list, "Восстановить чат с ${chatItem.title} из архива?", Snackbar.LENGTH_LONG)
+                .setAction(R.string.snackbar_undo_action) {
+                    viewModel.addToArchive(chatItem.id)
+                }
+                .show()
+        }
+        val touchHelper = ItemTouchHelper(touchCallback)
+        touchHelper.attachToRecyclerView(rv_archive_list)
+
+        with(rv_archive_list) {
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(this@ArchiveActivity)
+            addItemDecoration(divider)
+        }
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(ArchiveViewModel::class.java)
+        viewModel.getChatData().observe(this, Observer { chatAdapter.updateData(it) } )
     }
 }
